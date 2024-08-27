1401b4672b134e5994888471c9420a7251153d6f
Kamylla Lacerda <kamyllarlacerda@gmail.com>
2024-08-27 12:09:17 -0300

    Repositorios

---
+++ src/main/java/com/poc/diagnostico/repository/MunicipioRepository.java
@@ -0,0 +1,11 @@
+package com.poc.diagnostico.repository;
+
+import com.poc.diagnostico.entity.Municipio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MunicipioRepository extends JpaRepository<Municipio, UUID> {
+}
